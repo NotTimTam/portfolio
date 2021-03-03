8f3bffba4d0c490a3a7a39fc12d8ff5a2b064bc2
Timothy Phillips <Tphill180@west-mec.local>
2021-03-02 17:47:53 -0700

    Added projects area.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ $("#resume-drop").css("padding-left", "0px");
 
 // Menu collapse.
 $(document).on("scroll", function () {
-    if ($(document).width() < 991) {
+    if ($(document).width() < 991 && $(document).width() > 700) {
         if ($(document).scrollTop() > $("#resume").position().top-1) {
-            $("#resume-drop").css("height", "200px");
+            $("#resume-drop").css("height", "192.2px");
             $("#resume-drop").css("padding-left", "15px");
         } else {
             $("#resume-drop").css("height", "0px");
