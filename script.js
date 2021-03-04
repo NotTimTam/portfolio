3d0d383e312304985899998a232f0117372d68a6
Timothy Phillips <Tphill180@west-mec.local>
2021-03-03 17:26:54 -0700

    Upgraded projects tab.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 $("#resume-drop").css("height", "0px");
 $("#resume-drop").css("padding-left", "0px");
+$(".sidebar").css("left", "-64px");
 
-// Menu collapse.
-$(document).on("scroll", function () {
+function checkSidebar() {
     if ($(document).width() < 991 && $(document).width() > 700) {
         if ($(document).scrollTop() > $("#resume").position().top-1) {
             $("#resume-drop").css("height", "192.2px");
@@ -17,4 +17,14 @@ $(document).on("scroll", function () {
         $("#resume-drop").css("height", "0px");
         $("#resume-drop").css("padding-left", "0px");
     }
-});
+
+    if ($(document).width() < 700) {
+        $(".sidebar").css("left", "-64px");
+    } else {
+        $(".sidebar").css("left", "0px");
+    }
+}
+
+// Menu collapse.
+$(document).on("scroll", checkSidebar);
+$(window).on("resize", checkSidebar);
